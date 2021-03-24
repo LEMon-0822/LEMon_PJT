4245572817fc43db25b016a0f42e4ff15b8f33ec
이의민 <lem0822@gmail.com>
2021-03-24 17:48:40 +0900

    영상 침식 팽창 및 노이즈제거 후 캔디 사용해 경계선 검출

--- Img-Preprocessing.py
+++ Img-Preprocessing.py
@@ -1,24 +1,38 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("F:/rabbit cornea experiment/Rabbit collagen sheet/4 weeks after/#2/4 weeks sample #2_XZ.png", cv2.IMREAD_UNCHANGED)
+img = cv2.imread("F:/rabbit cornea experiment/Rabbit collagen sheet/2 weeks after/#8/2 weeks sample #8_XZ.png", cv2.IMREAD_UNCHANGED)
 img2 = img.copy
-k = cv2.getStructuringElement(cv2.MORPH_RECT,(4,4))
-img = cv2.fastNlMeansDenoisingColored(img,None,13,13,7,21)
-img = cv2.GaussianBlur(img, (3,3), 0)
-img = cv2.dilate(img, k)
-img_erod = cv2.erode(img, k)
+k1 = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+k2 = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 
 cv2.imshow("contour",img)
-cv2.imshow("erod",img_erod)
 cv2.waitKey(0)
 
-img2 =cv2.Canny(img,80, 100)
-img2_erod = cv2.Canny(img_erod,80,100)
+img = cv2.fastNlMeansDenoisingColored(img,None,12,12,7,13)
+#img = cv2.GaussianBlur(img, (3,3), 0)
+
+
+img = cv2.dilate(img, k1)
+cv2.imshow("contour",img)
+cv2.waitKey(0)
+
+
+img = cv2.erode(img, k1)
+cv2.imshow("contour",img)
+cv2.waitKey(0)
+
+cv2.imshow("contour",img)
+cv2.waitKey(0)
+#cv2.imshow("erod",img_erod)
+
+
+img2 =cv2.Canny(img, 50, 170)
+#img2_erod = cv2.Canny(img,40,80)
 
 
 cv2.imshow("contour",img2)
-cv2.imshow("erod", img2_erod)
+#cv2.imshow("erod", img2_erod)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
